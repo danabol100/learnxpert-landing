a702d0ccb02d6c4b68d55f9d201ad88f08bf64bb
Kyrylo Shkarupylo <danabol100@gmail.com>
2026-03-22 20:52:51 +0200

    bentfit section

--- js/index.js
+++ js/index.js
@@ -12,4 +12,18 @@ function load() {
       document.documentElement.toggleAttribute("data-menu-open");
     }
   });
+  const cards = document.querySelectorAll(".benefits__card");
+
+  let current = 0;
+
+  setInterval(() => {
+    // убираем active у всех
+    cards.forEach((card) => card.classList.remove("benefits__card--active"));
+
+    // добавляем текущей
+    cards[current].classList.add("benefits__card--active");
+
+    // переходим к следующей
+    current = (current + 1) % cards.length;
+  }, 2000); // 2 секунды
 }
